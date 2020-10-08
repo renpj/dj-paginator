343ac0821903b703afd239714c714b09203db880
renpj <openrpj@gmail.com>
2020-10-08 13:25:42 +0800

    Default theme Bootstrap4

--- paginator/templatetags/paginator_tags.py
+++ paginator/templatetags/paginator_tags.py
@@ -5,13 +5,19 @@ from paginator.paginators import paginate
 
 register = template.Library()
 
-PAGINATOR_THEME = getattr(settings, "PAGINATOR_THEME", "bootstrap")
+PAGINATOR_THEME = getattr(settings, "PAGINATOR_THEME", "bootstrap4")
 BASIC_THEME = 'paginator/{}/paginator.html'.format(PAGINATOR_THEME)
 BOOTSTRAP_THEME = 'paginator/bootstrap/paginator.html'
+BOOTSTRAP4_THEME = 'paginator/bootstrap4/paginator.html'
 
 
 @register.inclusion_tag(BOOTSTRAP_THEME, takes_context=True)
 def bootstrap_paginator(context):
+    return paginate(context)
+
+
+@register.inclusion_tag(BOOTSTRAP4_THEME, takes_context=True)
+def bootstrap4_paginator(context):
     return paginate(context)
 
 
